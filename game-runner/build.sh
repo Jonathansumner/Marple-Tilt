1daa1ae3e6a0586483103eea5a072cfe537296fd
2323851r <2323851r@student.gla.ac.uk>
2022-02-18 00:52:14 +0000

    Removed old submodules

--- game-runner/build.sh
+++ game-runner/build.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
-cd out/build ; make
+make -C external/matrix-api
+make -C external/mpu6050-api
+
+cmake -S . -B out/build
+
+make -C out/build
--- game-runner/configure.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-make -C external/matrix-api
-make -C external/mpu6050-api
-
-cmake -S . -B out/build
