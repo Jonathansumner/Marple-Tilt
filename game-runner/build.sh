3a0adafafc53ec053addedc35b51eb1ea53e05ba
Jonathan Sumner <jonathansumner98@gmail.com>
2022-02-19 15:51:50 +0000

    fix: return build.sh to local compilation

--- game-runner/build.sh
+++ game-runner/build.sh
@@ -7,7 +7,7 @@ sudo apt-get install libi2c0
 make -C external/matrix-api
 make -C external/mpu6050-api
 
-cmake --no-warn-unused-cli -DCMAKE_TOOLCHAIN_FILE=cross.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -B out/build -G "Unix Makefiles"
-
+#cmake --no-warn-unused-cli -DCMAKE_TOOLCHAIN_FILE=cross.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -B out/build -G "Unix Makefiles"
+cmake -S . -B out/build
 
 make -C out/build
