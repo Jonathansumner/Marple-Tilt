cdc593d6b3dc266303eddf88fba57a510b1cd95b
Jonathan sumner <jonathansumner98@gmail.com>
2022-02-21 20:35:18 +0000

    feat: shape rendering allows brightness as optional parameter

--- game-runner/graphics/shapes.h
+++ game-runner/graphics/shapes.h
@@ -3,10 +3,10 @@
 
 #endif //MARPLETILT_SHAPES_H
 
-void drawRect(int start_x, int start_y, int w, int h, vector<int>* rgb, rgb_matrix::Canvas *canvas) {
+void drawRect(int start_x, int start_y, int w, int h, vector<int>* rgb, rgb_matrix::Canvas *canvas, float brightness=1) {
     for (int w_curr = 0; w_curr < w; w_curr++) {
         for (int h_curr = 0; h_curr < h; h_curr++) {
-            canvas->SetPixel(start_x + w_curr, start_y + h_curr, rgb->at(0), rgb->at(1), rgb->at(2));
+            canvas->SetPixel(start_x + w_curr, start_y + h_curr, round(rgb->at(0)*brightness), round(rgb->at(1)*brightness), round(rgb->at(2)*brightness));
         }
     }
 }
