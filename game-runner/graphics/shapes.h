49749e1a384111793dab06c6e488785cba956744
Jonathan sumner <jonathansumner98@gmail.com>
2022-02-21 20:20:42 +0000

    fix: move and render test

--- game-runner/graphics/shapes.h
+++ game-runner/graphics/shapes.h
@@ -6,8 +6,8 @@
 //#include <led-matrix.h>
 
 void drawRect(int start_x, int start_y, int end_x, int end_y, vector<int>* rgb, rgb_matrix::Canvas *canvas) {
-    for (int w_curr = 0; w_curr < end_x; w_curr++) {
-        for (int h_curr = 0; h_curr < end_y; h_curr++) {
+    for (int w_curr = 0; w_curr < end_x-start_x; w_curr++) {
+        for (int h_curr = 0; h_curr < end_y-start_y; h_curr++) {
             canvas->SetPixel(start_x + w_curr, start_y + h_curr, rgb->at(0), rgb->at(1), rgb->at(2));
         }
     }
