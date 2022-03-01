7e2fbcab80c4a1e0624b4dc6de88043dc2f5de84
2323851r <2323851r@student.gla.ac.uk>
2022-03-01 16:58:38 +0000

    Feat: Added image display as function

---
+++ game-runner/graphics/images.h
@@ -0,0 +1,4 @@
+#ifndef MARPLETILT_IMAGES_H
+#define MARPLETILT_IMAGES_H
+
+void drawImage(const char *filename, int duration, char *argv[], Canvas *canvas);
--- game-runner/main.cpp
+++ game-runner/main.cpp
@@ -8,6 +8,7 @@
 #include "graphics/gfx.h"
 #include "graphics/shapes.h"
 #include "MPU6050.h"
+#include "images.h"
 
 #include <exception>
 #include <Magick++.h>
@@ -102,7 +103,22 @@ int main(int argc, char *argv[]) {
     defaults.hardware_mapping = "regular";  // or e.g. "adafruit-hat"
     defaults.rows = 64;
     defaults.cols = 64;
-    defaults.disable_hardware_pulsing = true;
+    defaults.disable_hardwvoid ShowAnimatedImage(const ImageVector &images, RGBMatrix *matrix)
+    {
+        FrameCanvas *offscreen_canvas = matrix->CreateFrameCanvas();
+        while (!interrupt_received)
+        {
+            for (const auto &image : images)
+            {
+                if (interrupt_received)
+                    break;
+                CopyImageToCanvas(image, offscreen_canvas);
+                offscreen_canvas = matrix->SwapOnVSync(offscreen_canvas);
+                usleep(image.animationDelay() * 10000); // 1/100s converted to usec
+            }
+        }
+    }
+    are_pulsing = true;
     defaults.chain_length = 1;
     defaults.parallel = 1;
     defaults.show_refresh_rate = true;
@@ -126,9 +142,9 @@ int main(int argc, char *argv[]) {
         std::cout << "Gyroscope angles: X: " << a << ", Y: " << b << ", Z: " << c << "\n";
         update();
         render(canvas);
-
-        //After game updates
-        gettimeofday(&t, nullptr);
+        @id : ms - vscode.cpptools - extension - pack
+                                                     // After game updates
+                                                     gettimeofday(&t, nullptr);
         timestamp2 = t.tv_sec*1000L + (t.tv_usec/1000L);
         elapsed_time = timestamp2 - timestamp1;
         if (elapsed_time < frame_time) {
@@ -136,6 +152,9 @@ int main(int argc, char *argv[]) {
         }
         canvas->Clear();
     }
+
+    drawImage("img/logo.ppm", 5, argv, canvas);
+
     std::cout << "Program terminated\n";
     canvas->Clear();
     delete canvas;
