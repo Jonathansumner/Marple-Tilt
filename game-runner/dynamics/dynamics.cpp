6ce70c66403c2fce62d8e979a80cc4e8c40d0dd3
Jonathan sumner <jonathansumner98@gmail.com>
2022-03-01 18:00:36 +0000

    fix: modify comments

--- game-runner/dynamics/dynamics.cpp
+++ game-runner/dynamics/dynamics.cpp
@@ -19,12 +19,12 @@ void updateMarple(Marple* marple, MPU6050* gyro) {
  *          100 -> -100 : (1p/s)/s -> -1p/s/s
  * apply the acceleration
  *          position + (velocity + acceleration) = new position
- */
 
-/*once new position obtained, check for collisions
+ once new position obtained, check for collisions
     *if in new position there is a wall
         *check if marple new position would touch wall or be inside wall
             *if inside wall, reposition so it only touches wall
             *if touching wall invert the acceleration so it "bounces off" wall
         *check if marple new position would touch hole
             *if touches hole, set new psotion to start position to teleport it back to the start.
+            */
