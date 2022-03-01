8b1382e9fc753130a3fa7209e6ce4bac11e8bc9a
Jonathan Sumner <jonathansumner98@gmail.com>
2022-03-01 16:17:33 +0000

    chore: create initial structure

---
+++ game-runner/dynamics/dynamics.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jonat on 01/03/2022.
+//
+
+#include "dynamics.h"
---
+++ game-runner/dynamics/dynamics.h
@@ -0,0 +1,8 @@
+//
+// Created by Jonat on 01/03/2022.
+//
+
+#ifndef MARPLE_TILT_DYNAMICS_H
+#define MARPLE_TILT_DYNAMICS_H
+
+#endif //MARPLE_TILT_DYNAMICS_H
