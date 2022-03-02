93da24cd9a00c0877029ad9b83727f26213bf342
2323851r <2323851r@student.gla.ac.uk>
2022-03-02 23:26:13 +0000

    Fix: Fixed small errors with implementation

--- game-runner/main.cpp
+++ game-runner/main.cpp
@@ -142,9 +142,11 @@ int main(int argc, char *argv[]) {
 
     drawImage("img/new_logo.ppm", 5, argv, canvas);
 
-    rgb_matrix::Font font = "img/5x8.bdf";
+    rgb_matrix::Font font;
+    font.LoadFont("img/5x8.bdf")
+
     Color fontColor(0,40,100);
-    drawText(canvas, font, 10, 10, fontColor, NULL, "Marple",0);
+    rgb_matrix::drawText(canvas, font, 10, 10, fontColor, NULL, "Marple",0);
 
 
     std::cout << "Program terminated\n";
